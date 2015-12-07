e571d7e1c188d186ffa09fce8101795884091348
james.liu <jupiturliu@gmail.com>
2015-12-06 22:47:34 -0800

    code refactoring

--- mk/nvme.unittest.mk
+++ mk/nvme.unittest.mk
@@ -1,44 +1,11 @@
-#
-#  BSD LICENSE
-#
-#  Copyright(c) 2010-2015 Intel Corporation. All rights reserved.
-#  All rights reserved.
-#
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions
-#  are met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in
-#      the documentation and/or other materials provided with the
-#      distribution.
-#    * Neither the name of Intel Corporation nor the names of its
-#      contributors may be used to endorse or promote products derived
-#      from this software without specific prior written permission.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
+OMNIOS_ROOT_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))/..
+NVME_DIR := $(OMNIOS_ROOT_DIR)/lib/nvme
 
-SPDK_ROOT_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))/..
-NVME_DIR := $(SPDK_ROOT_DIR)/lib/nvme
-
-include $(SPDK_ROOT_DIR)/mk/spdk.common.mk
+include $(OMNIOS_ROOT_DIR)/mk/omnios.common.mk
 
 C_SRCS = $(TEST_FILE) $(OTHER_FILES)
 
-CFLAGS += -I$(SPDK_ROOT_DIR)/lib -include $(SPDK_ROOT_DIR)/test/lib/nvme/unit/nvme_impl.h
+CFLAGS += -I$(OMNIOS_ROOT_DIR)/lib -include $(OMNIOS_ROOT_DIR)/test/lib/nvme/unit/nvme_impl.h
 
 LIBS += -lcunit -lpthread
 
@@ -55,4 +22,4 @@ clean:
 %.o: $(NVME_DIR)/%.c %.d $(MAKEFILE_LIST)
 	$(COMPILE_C)
 
-include $(SPDK_ROOT_DIR)/mk/spdk.deps.mk
+include $(OMNIOS_ROOT_DIR)/mk/omnios.deps.mk
